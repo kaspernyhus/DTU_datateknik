4aa14b14d97268e678bc772908301e6b503fbc8e
Kasper Nyhus <kanyhus@gmail.com>
2020-02-25 00:26:36 +0100

    pinmode

---
+++ setPinMode_2560/src/main.c
@@ -0,0 +1,24 @@
+/*
+ * 
+ * Kasper Nyhus Kaae
+ * s195087
+ */ 
+
+#include <avr/io.h>
+
+void setPinMode(int pin, char mode);
+
+int main(void)
+{
+
+	while (1)
+    {
+		
+    }
+}
+
+
+void setPinMode(int pin, char mode) {
+    
+
+}
