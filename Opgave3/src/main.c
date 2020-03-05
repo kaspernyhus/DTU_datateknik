de99d1db13a737898ce2320a785f650fcf3c1c01
Kasper Nyhus <kanyhus@gmail.com>
2020-03-05 11:46:24 +0100

    print_dec added

--- Opgave3/src/main.c
+++ Opgave3/src/main.c
@@ -47,10 +47,27 @@ int getRawData() {
 }
 
 
+void print_bin(int c, int bits, int row) {
+  char c_buffer;
+  int XY = 15 - ((16 - bits) / 2); // prints in the center of the screen
+
+  for (int i=0; i<bits; i++) {
+    c_buffer = '0' + !!(c & (1 << i));
+    sendCharXY(c_buffer, row, XY-i);
+  }
+}
+
+
+void print_dec(int dec, int x, int y) {
+  char str[20];
+  sprintf(str, "%d", dec);
+  sendStrXY(str, x, y);
+}
+
 
 void show_temp(int data) {
-  int decimal = binaryToDecimal(data);
-  sendStrXY(decimal, 4, 5);
+  data = data * 0.25;
+  print_dec(data, 4,5);
 }
 
 
