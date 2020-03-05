f146b10f8e5322e9dd6d92361353b0799dc861a3
Kasper Nyhus <kanyhus@gmail.com>
2020-03-05 11:29:44 +0100

    formatting

--- bin-deci/src/main.c
+++ bin-deci/src/main.c
@@ -18,13 +18,13 @@ void init_OLED() {
 
 
 void print_bin(int c, int bits, int row) {
-char c_buffer;
-int XY = 15 - ((16 - bits) / 2); // prints in the center of the screen
+  char c_buffer;
+  int XY = 15 - ((16 - bits) / 2); // prints in the center of the screen
 
-for (int i=0; i<bits; i++) {
-c_buffer = '0' + !!(c & (1 << i));
-sendCharXY(c_buffer, row, XY-i);
-}
+  for (int i=0; i<bits; i++) {
+    c_buffer = '0' + !!(c & (1 << i));
+    sendCharXY(c_buffer, row, XY-i);
+  }
 }
 
 
