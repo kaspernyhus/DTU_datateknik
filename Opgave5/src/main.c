80bf9799eadcba113da1794e07f49947c51622fb
Kasper Nyhus <kanyhus@gmail.com>
2020-05-07 21:23:42 +0200

    statedef

--- Opgave5/src/main.c
+++ Opgave5/src/main.c
@@ -39,54 +39,8 @@ volatile char data = 0;
 
 
 //------------------------------------------------------------------------
-//													STATE MACHINE
+//													STATE MACHINE ACTIONs
 //------------------------------------------------------------------------
-
-//Menu init message
-char message[]= "Indtast:\r a to set PWM MIN value (0) \r b to set PWM MAX value (255)\r\0";
-
-//Action function difinitions
-void noAction(void);
-void init_MIN_MAX(void);
-void updateMIN(void);
-void updateMAX(void);
-void saveChar(void);
-void resetBuffer(void);
-
-/*
-CurSta		noinput							"a"						"b"						"Enter"
-init			init_s,init_MIN_MAX	MIN,noAction	MAX,noAction	listen,noAction
-listen		listen,noAction			MIN,noAction	MAX,noAction	listen,noAction
-MIN				MIN_val,saveChar		MIN,noAction	MAX,noAction	listen,noAction
-MIN_val		MIN_val,saveChar		MIN,noAction	MAX,noAction	listen,updateMIN
-MAX				MAX_val,saveChar		MIN,noAction	MAX,noAction	listen,noAction
-MAX_val		MAX_val,saveChar		MIN,noAction	MAX,noAction	listen,updateMAX
-*/
-
-stateElement stateMatrix[6][4] = {
-	{ 	{init_s,init_MIN_MAX},{MIN,noAction}, 	{MAX,noAction}, 	{listen,noAction} 	},
-	{ 	{listen,noAction}, 		{MIN,noAction}, 	{MAX,noAction}, 	{listen,noAction} 	},
-	{ 	{MIN_val,saveChar},		{MIN,noAction}, 	{MAX,noAction}, 	{listen,noAction} 	},
-	{ 	{MIN_val,saveChar},		{MIN,noAction}, 	{MAX,noAction}, 	{listen,updateMIN} 	},
-	{ 	{MAX_val,saveChar},		{MIN,noAction}, 	{MAX,noAction}, 	{listen,noAction} 	},
-	{ 	{MAX_val,saveChar},		{MIN,noAction}, 	{MAX,noAction}, 	{listen,updateMAX} 	}
-};
-
-//initial event
-event	eventOccured = NILEVENT;
-void	stateEval(event e);
-
-//initial state
-state currentState=init_s;
-
-//state action control based on events
-void stateEval(event e)
-{
-    stateElement stateEvaluation = stateMatrix[currentState][e];     //a table look-up to fetch the relevant next state and the related function pointer
-    currentState= stateEvaluation.nextState;
-    (*stateEvaluation.actionToDo)();                               //the execution
-}
-
 
 // ACTION funcitons
 void noAction(void) {
@@ -220,7 +174,6 @@ int main(void) {
 //------------------------------------------------------------------------
 //											INTERRUPT SERVICE ROUTINES
 //------------------------------------------------------------------------
-
 
 ISR(USART0_RX_vect) { // UART0 receive
 	data=UDR0;
